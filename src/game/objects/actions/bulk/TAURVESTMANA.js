0562b303c6b9ae5c87ee68d66070ad52f255fc67
Matthew R Bond <mothwentbad@gmail.com>
2019-08-24 01:57:46 -0400

    Lower cost/power of TAURVESTMANA.  Needs rename to avoid confusion. :(

--- src/game/objects/actions/bulk/TAURVESTMANA.js
+++ src/game/objects/actions/bulk/TAURVESTMANA.js
@@ -11,18 +11,23 @@ class TAURVESTMANA {
       // filename: 'HEAL',
       name: 'Taurvest Mana',
       type: 'both',
-      desc: 'Heal self for 1x (MAGIC + MELEE + RANGED) HP.',
-      cost: 2,
-      targetRules: ['SELF'],
+      desc: 'Heal self for 0.5x (MAGIC + MELEE + RANGED) HP, and activate ' +
+        `guard.`,
+      cost: 1,
+      targetRules: ['SELF', 'SELF'],
       effects: [
         {
           NAME: 'HEAL',
           name: 'heal',
           scale: {
-            MAGIC: 1,
-            MELEE: 1,
-            RANGED: 1
+            MAGIC: 0.5,
+            MELEE: 0.5,
+            RANGED: 0.5
           }
+        },
+        {
+          NAME: 'GUARD',
+          name: 'guard'
         }
       ]
     })
